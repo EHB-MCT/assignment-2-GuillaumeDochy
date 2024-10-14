d8cf935a4293695b32be4eecf8b7961cd01ad7fc
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2024-10-14 15:48:36 +0200

    Initial setup
    Initial setup for the project with template files created through Xcode as well as creating a good gitignore

---
+++ Weather-Data/Weather-Data/Weather_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_DataApp.swift
+//  Weather-Data
+//
+//  Created by Guillaume Dochy on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
